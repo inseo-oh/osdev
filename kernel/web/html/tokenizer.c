e0dfc8badd823a8d7d5804b7f8333ac30473d55d
Inseo Oh <dhdlstjtr@gmail.com>
2024-01-09 23:23:55 +0900

    forgot that it's 2024

--- kernel/web/html/tokenizer.c
+++ kernel/web/html/tokenizer.c
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: (c) 2023 Inseo Oh <dhdlstjtr@gmail.com>
+// SPDX-FileCopyrightText: (c) 2024 Inseo Oh <dhdlstjtr@gmail.com>
 //
 // SPDX-License-Identifier: BSD-2-Clause
 #include "html.h"
